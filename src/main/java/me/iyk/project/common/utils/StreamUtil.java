926c4878daaff1a3e913cc34448ccd93dde2d26e
Echos <echos.yan@qq.com>
2020-11-13 11:43:57 +0800

    add JacksonUtil & StreamUtil

---
+++ src/main/java/me/iyk/project/common/utils/StreamUtil.java
@@ -0,0 +1,28 @@
+package me.iyk.project.common.utils;
+
+import lombok.NonNull;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+/**
+ * java8 stream 的一些用法的封装
+ *
+ * @author Echos
+ * @date 2020/11/13
+ */
+public class StreamUtil {
+
+    public static <T, K> Map<K, T> list2Map(@NonNull Collection<T> list, @NonNull Function<? super T, K> keyFunc) {
+        return list.stream().collect(Collectors.toMap(keyFunc, Function.identity(),
+                (u, v) -> {
+                    throw new IllegalStateException(String.format("Multiple entries with same key,%s=%s,%s=%s",
+                            keyFunc.apply(u), u,
+                            keyFunc.apply(v), v));
+                },
+                HashMap::new));
+    }
+}
